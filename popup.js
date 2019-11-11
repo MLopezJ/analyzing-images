02f7faed00e4534fc90734408f257d28aff1aa7f
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2019-11-11 10:25:01 -0600

    added dynamic message in button when the info is displayed or not

--- popup.js
+++ popup.js
@@ -135,16 +135,18 @@ var callback = async function (imagesFromBrowser) {
         document.body.appendChild(divError);
 
         var acc = document.getElementsByClassName("accordion");
+        
         for (i = 0; i < acc.length; i++) {
             acc[i].addEventListener("click", function() {
                 this.classList.toggle("active");
                 var panel = this.nextElementSibling;
                 if (panel.style.display === "block") {
- 
                     panel.style.display = "none";
+                    this.innerHTML = "+ Info"
                 } 
                 else { 
                     panel.style.display = "block";
+                    this.innerHTML = "- Info"
                 }
             });
         }
